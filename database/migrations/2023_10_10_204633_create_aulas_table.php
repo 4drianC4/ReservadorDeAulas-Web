98e005d13ee66e820ead11de8fa460794bdc965a
OliverPozo <oliver.pozo10@gmail.com>
2023-10-10 18:38:27 -0400

    Creacion de la Base de datos

--- database/migrations/2023_10_10_204633_create_aulas_table.php
+++ database/migrations/2023_10_10_204633_create_aulas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateAulasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('aulas', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('nombre');
+            $table->string('ubicacion');
+            $table->string('capacidad');
+            $table->string('tipo');
+            $table->string('descripcion');
+            $table->string('estado');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('aulas');
     }
 }
