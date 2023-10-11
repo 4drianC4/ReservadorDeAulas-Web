d0eb8c09264113512cb818042858e37ec66dfb91
4drianC4 <camararequeadrianrafael@gmail,com>
2023-10-10 17:07:33 -0700

    front

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\File;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $path = public_path('assets/index.html');
+    if(File::exists($path)){
+        return File::get($path);
+    }
+    abort(404);
 });
